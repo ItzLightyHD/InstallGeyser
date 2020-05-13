60cf20587b761cb08ec5f41f0e57e9e1ec942c86
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-13 16:59:15 +0200

    Now start Geyser scripts are writed directly into the installer

--- installgeyser.bat
+++ installgeyser.bat
@@ -8,7 +8,8 @@ del "Geyser.jar" >nul 2>&1
 del "start.bat" >nul 2>&1
 echo [2/4] Downloading Geyser...
 powershell -command Invoke-WebRequest https://ci.nukkitx.com/job/GeyserMC/job/Geyser/job/%BRANCH%/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar -OutFile Geyser.jar
-echo [3/4] Getting start.bat...
-powershell -command Invoke-WebRequest https://raw.githubusercontent.com/ItzLightyHD/InstallGeyser/master/start.bat -OutFile start.bat
+echo [3/4] Writing start.bat...
+@echo @echo off>start.bat
+@echo java -Xmx1024M -Xms1024M -jar Geyser.jar>>start.bat
 echo [4/4] Done! Now you can start Geyser by opening start.bat
 echo [*] If you want, you can delete the script too.
